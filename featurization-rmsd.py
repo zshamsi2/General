addcf05700df79504762c6a6983a807d4c0cc9aa
Zahra Shamsi <zshamsi2@illinois.edu>
2015-10-23 15:33:22 -0500

    Update featurization-rmsd.py

--- featurization-rmsd.py
+++ featurization-rmsd.py
@@ -3,6 +3,9 @@ import mdtraj as md
 import glob
 
 ref_2hyy=md.load('2HYY.pdb')
-t = md.load('stripedTrj/MD1-strTrj/2HYY_2_md1-01.mdcrd', top='2HYY.prmtop')
-rmsds = md.rmsd(t, ref_2hyy, frame=0)
-np.save('tst.npy', rmsds)
+count=0
+for file in glob.glob('stripedTrj/MD1-strTrj/2HYY*')
+  t = md.load(file, top='2HYY.prmtop')
+  rmsds = md.rmsd(t, ref_2hyy, frame=0)
+  np.save(str(count)+'.npy', rmsds)
+  count=count+1
