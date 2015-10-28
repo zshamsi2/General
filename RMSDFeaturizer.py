f855399bfc7d18a679d1d0cdafa67f56aafd4252
Zahra Shamsi <zshamsi2@illinois.edu>
2015-10-28 11:03:56 -0500

    Create RMSDFeaturizer.py

---
+++ RMSDFeaturizer.py
@@ -0,0 +1,13 @@
+import numpy as np
+import mdtraj as md
+import glob
+
+# reference structure for calculating RMSD
+ref=md.load('2HYY.pdb')
+count=0
+for file in glob.glob('stripedTrj/MD1-strTrj/*.dcd'):
+  a = file.split('/')[-1]
+  name = a.split('.')[0]
+  t = md.load(file, top='2HYY.prmtop')
+  rmsds = md.rmsd(t, ref, frame=0)
+  np.save(name+'.npy', rmsd)
