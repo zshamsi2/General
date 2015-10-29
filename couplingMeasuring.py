c9d334c65690efb4ad714adc031bab0d22da571c
Zahra Shamsi <zshamsi2@illinois.edu>
2015-10-29 18:35:18 -0500

    Update couplingMeasuring.py

--- couplingMeasuring.py
+++ couplingMeasuring.py
@@ -7,7 +7,7 @@ ci = np.loadtxt('ADRB2_HUMAN_ECScores.csv', delimiter = ',')
 maxP = ci[0][2]
 eps = []
 for i in range(len(ci)):
-    if ci[i][2] > (maxP*0.5):
+    if ci[i][2] > (maxP*0.1):
     #evolutionary pairs
         ep = [ci[i][0], ci[i][1], ci[i][2]]
         eps.append(ep)
@@ -39,10 +39,40 @@ for i in range(len(eps)):
 
 t1=md.load('3SN6-R.pdb')
 t2=md.load('2RH1.pdb')
-dist1=md.compute_contacts(t1, contacts=eps, scheme='closest')
-dist2=md.compute_contacts(t2, contacts=eps, scheme='closest')
 
-deltaDist = [[dist1[0][0][i]-dist2[0][0][i], eps[i][2]] for i in range(len(dist1[0][0]))]
+eps1 = [[eps[i][0], eps[i][1]] for i in range(len(eps))]
+dist1=md.compute_contacts(t1, contacts=eps1, scheme='closest')
+dist2=md.compute_contacts(t2, contacts=eps1, scheme='closest')
 
-plt.plot(deltaDist)
+deltaDist = [dist1[0][0][i]-dist2[0][0][i] for i in range(len(dist1[0][0]))]
+x = [eps[i][2] for i in range(len(eps))]
+plt.scatter(x , np.absolute(deltaDist))
+plt.savefig('fig1.png')
 plt.show()
+
+####################################################################################################################
+# calculating dihedrals
+####################################################################################################################
+top1 = md.load('3SN6-R.pdb').topology
+top2 = md.load('2RH1.pdb').topology
+
+dhdrls101 = []
+dhdrls102 = []
+dhdrls201 = []
+dhdrls202 = []
+
+for i in range(len(eps)):
+    atoms101 = top1.select("type == C and resid " + str(eps[i][0]))
+    atoms102 = top1.select("type == C and resid " + str(eps[i][1]))
+    atoms201 = top2.select("type == C and resid " + str(eps[i][0]))
+    atoms202 = top2.select("type == C and resid " + str(eps[i][1]))
+    
+    dhdrl101 = md.compute_dihedrals(t1, atoms101)
+    dhdrl102 = md.compute_dihedrals(t1, atoms102)
+    dhdrl201 = md.compute_dihedrals(t2, atoms201)
+    dhdrl202 = md.compute_dihedrals(t2, atoms202)
+    
+    dhdrls101.append(dhdrl101)
+    dhdrls102.append(dhdrl102)
+    dhdrls201.append(dhdrl201)
+    dhdrls202.append(dhdrl202)
