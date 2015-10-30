3dd9a3736839a7c2413c2ad8e7a77eeb18e61187
Zahra Shamsi <zshamsi2@illinois.edu>
2015-10-30 12:24:11 -0500

    Update RMSDFeaturizer.py

--- RMSDFeaturizer.py
+++ RMSDFeaturizer.py
@@ -10,4 +10,6 @@ for file in glob.glob('stripedTrj/MD1-strTrj/*.dcd'):
   name = a.split('.')[0]
   t = md.load(file, top='2HYY.prmtop')
   rmsds = md.rmsd(t, ref, frame=0)
-  np.save(name+'.npy', rmsd)
+  rmsds1 = [[j] for j in rmsds]
+  rmsds2 = np.array(rmsds1)
+  np.save(name+'.npy', rmsds2)
