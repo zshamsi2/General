6080b7d61a628c9d03890c17f3d1238133cd0083
Zahra Shamsi <zshamsi2@illinois.edu>
2015-12-09 13:33:36 -0600

    Update seq-edit01.py

--- seq-edit01.py
+++ seq-edit01.py
@@ -5,3 +5,9 @@ unfrag = ['>'+line for line in lines if line.find('Fragment')==-1]
 frag = ['>'+line for line in lines if line.find('Fragment')!=-1]
 np.savetxt('test-frag.txt', frag, delimiter=" ", fmt="%s") 
 np.savetxt('test-unfrag.txt', unfrag, delimiter=" ", fmt="%s") 
+
+import numpy as np
+f=open('list')
+lines = f.read().split('\n')
+frag = ['AS2'+line for line in lines]
+np.savetxt('AS2_list', frag, delimiter=" ", fmt="%s") 
