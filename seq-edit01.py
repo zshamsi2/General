dcac2d8645b986c7cf6724fea71905642f2bca87
Zahra Shamsi <zshamsi2@illinois.edu>
2015-12-10 11:06:44 -0600

    Update seq-edit01.py

--- seq-edit01.py
+++ seq-edit01.py
@@ -7,7 +7,12 @@ np.savetxt('test-frag.txt', frag, delimiter=" ", fmt="%s")
 np.savetxt('test-unfrag.txt', unfrag, delimiter=" ", fmt="%s") 
 
 import numpy as np
-f=open('list')
+f=open('str')
 lines = f.read().split('\n')
-frag = ['AS2'+line for line in lines]
-np.savetxt('AS2_list', frag, delimiter=" ", fmt="%s") 
+frag = ['AS1'+line for line in lines]
+np.savetxt('AS1_list', frag, delimiter=" ", fmt="%s") 
+
+for i in `seq 0 500`
+do
+cp SampleR4_${i}.prmtop AS1_SampleR4_${i}.prmtop
+done
