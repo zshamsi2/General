b0e01d19c8d4f3caa6dfa6d75b7da0997a83011c
Zahra Shamsi <zshamsi2@illinois.edu>
2016-02-22 14:25:42 -0600

    Update mkStarting.py

--- Adaptive-Sampling/mkStarting.py
+++ Adaptive-Sampling/mkStarting.py
@@ -3,20 +3,33 @@ import pickle
 import glob 
 import mdtraj as md
 import adaptiveSamplingMSM as ad
+from msmbuilder.msm import MarkovStateModel
 
-msm='MSM10-500.pkl'
-cl='clustering.pkl'
-n_samples = 100
-Trjs = '../*.mdcrd'
-top = 'pNRTapo-strip.pdb'
+cl = 'clustering_2OIQ_db.pkl'
+n_samples = 50
+Trjs = 'MD*-strTrj/*.mdcrd' # unstriped trajs!
+top = '2OIQ_db.prmtop'
+name_round = 'md3'
+name_sys = '2OIQ_db_'
+method = 'leastPop'
 
+def findTop(trjN):
+	a = trjN.split('/')[-1]
+	b = a.split('_md')[0]
+	top = b + '.prmtop'
+	#top = a.split('.')[0] + '.prmtop'
+	return top
+	
+	
 cluster = pickle.load(open(cl,'rb'))
 clL = cluster.labels_
-msm = io.load(msm)
+
+msm = MarkovStateModel(lag_time=10,n_timescales=10)
+msm.fit_transform(clL)
+
 trjs = clL
 N = n_samples
-inits = ad.findStarting(trjs, N, method='leastPop')
-
+inits = ad.findStarting([trjs], N, method=method)
 
 T = []
 for trj in sorted(glob.glob(Trjs)):
@@ -26,8 +39,12 @@ count = 0
 for init in inits:
 	structure = msm.draw_samples(clL, 1)[init]
 	print structure
+	top = findTop(T[structure[0][0]])
+	print top
 	f = md.load(T[structure[0][0]], top=top, frame=structure[0][1])
-	f.save_pdb(str(count)+'.pdb')
+	f.save_pdb(name_sys+str(count)+'_'+name_round+'.pdb')
+	f.save_pdb(name_sys+str(count)+'_'+name_round+'.prmtop')
+	f.save_pdb(name_sys+str(count)+'_'+name_round+'.mdcrd')
 	count = count+1	
 
 	print count 
