f467d7487591f52fc48814e4e124ac5eef0616f5
Zahra Shamsi <zshamsi2@illinois.edu>
2016-03-17 19:08:19 -0500

    Update main-adptvSamp.py

--- Adaptive-Sampling/main-adptvSamp.py
+++ Adaptive-Sampling/main-adptvSamp.py
@@ -30,12 +30,31 @@ T = []
 for trj in glob.glob('rawTrj/MD1-rwTrj/*.mdcrd'):
 	T.append(trj)
 T.sort()
-# Write the output file, which have the information about population of each cluster, 
-# trajectory name and frame number of corresponding frame 	
-adptvSampling.writeOPF_lessPop(l, T, myn_clusters, n_samples)
 
-# Based on information in output file, build the cpptraj input file, as you give it the topology name, it should be 
-# common for all trajectories
+
+# findStarting(trjs, N, method='random')
+N = n_samples
+trjs = l
+inits = ad.findStarting(trjs, N, method='leastcount')
+msm=MarkovStateModel(lag_time=1, n_timescales=10)
+msm.fit_transform(cl.labels_)
+
+OPF = []
+for i in range(n_samples):
+	init = msm.draw_samples(inits[i], 1)
+	traj = T[init[0][0]]
+	fram = init[0][1]
+	OPF.append({'traj':T[trj], 'frame':frm})
+json.dump(OPF, open("ClsInf.txt",'w'))
+
 
 topFile='mytopfile.top'
 adptvSampling.CpptrajInGen()
+
+
+
+# Write the output file, which have the information about population of each cluster, 
+# trajectory name and frame number of corresponding frame 	
+# adptvSampling.writeOPF_lessPop(l, T, myn_clusters, n_samples)
+# Based on information in output file, build the cpptraj input file, as you give it the topology name, it should be 
+# common for all trajectories
