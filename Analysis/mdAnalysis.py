84a94311b6e246745c35e380e1cd4aa8dd29836a
Zahra Shamsi <zshamsi2@illinois.edu>
2018-11-19 16:31:35 -0600

    Create mdAnalysis.py

---
+++ Analysis/mdAnalysis.py
@@ -0,0 +1,83 @@
+import numpy as np
+import mdtraj as md
+from matplotlib import pyplot as plt
+
+
+class mdAnalysis:
+
+        def __init__(self):
+
+        return
+        
+        def couplingMeasuring(self):
+        """
+        """
+        
+        
+        return
+        
+        def extractClustersPDB(self):
+        """
+        """
+        
+        
+        return
+        
+        def extractClustersPDB_Cpptraj(self):
+        """
+        """
+        
+        
+        return
+        
+        def extractClustersPDB_multiProcessor(self):
+        """
+        """
+        
+        
+        return
+        
+        def minIonDist(self):
+        """
+        """
+        
+        
+        return
+        
+        def mkCluster(self):
+        """
+        """
+        
+        
+        return
+        
+        def mkSnapshots(self):
+        """
+        """
+        
+        
+        return
+        
+        def mkSnapshots_Cpptrj(self):
+        """
+        """
+        
+        
+        return
+        
+        def mkSnapshots_multiProcessor(self):
+        """
+        """
+        
+        
+        return
+        
+        def msm_timescale_plt(self):
+        """
+        """
+        
+        
+        return
+
+        
+        
