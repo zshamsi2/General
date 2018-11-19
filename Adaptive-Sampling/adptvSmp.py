412134d585a1c815faeee4dae04d1fdbbb1efbfc
Zahra Shamsi <zshamsi2@illinois.edu>
2018-11-19 16:14:08 -0600

    Update adptvSmp.py

--- Adaptive-Sampling/adptvSmp.py
+++ Adaptive-Sampling/adptvSmp.py
@@ -3,7 +3,10 @@ import mdtraj as md
 import glob
 from msmbuilder.utils import io
 import msmbuilder.cluster
+from msmbuilder.msm import MarkovStateModel
 import pickle
+import adaptiveSamplingMSM as ad
+
 
 class adptvSmp:
   """
@@ -66,6 +69,52 @@ class adptvSmp:
                 io.dump(states,'clustering_'+name_sys+'_db.pkl')
                 return 
         
-
+        def findTop(self, trjN):
+	        roundN = trjN.split('/')[1][0:3]
+	        a = trjN.split('/')[-1]
+	        topN = a.split('_md')[0]
+	        rawtop = 'rawTrj/'+roundN+'-rwTop/'+topN+'.prmtop'
+	        return rawtop
+	
+        def findRawtrj(self, trjN):
+        	roundN = trjN.split('/')[1][0:3]
+        	sysName = trjN.split('/')[-1]
+        	rawTrj = 'rawTrj/'+roundN+'-rwTrj/'+sysName
+        	return rawTrj
         
-    
+        def cppGen(self, cl, n_samples):
+                cluster = pickle.load(open(cl,'rb'))
+                clL = cluster.labels_
+
+                msm = MarkovStateModel(lag_time=10,n_timescales=10)
+                msm.fit_transform(clL)
+                trjs = clL
+                N = n_samples
+                inits = ad.findStarting([trjs], N, method=method)
+                T = []
+                for trj in sorted(glob.glob(Trjs)):
+	                T.append(trj)
+                count = 0
+                for init in inits:
+                	structure = msm.draw_samples(clL, 1)[init]
+                	print structure
+                	top = findTop(T[structure[0][0]])
+                	rawTrj = findRawtrj(T[structure[0][0]])
+                	print(top)
+	                frame = structure[0][1]
+	                newTop = name_sys+str(count)+'_'+name_round+'.prmtop'
+	                newrst = name_sys+str(count)+'_'+name_round+'-00.rst'
+	                f = open('cppASample_'+str(count)+'.in', 'w')
+                        f.write('parm ' + top + '\n')
+                        f.write('trajin ' + rawTrj  + '\n')
+                        f.write('parmbox alpha 90 beta 90 gamma 90\n')
+                        f.write('trajout ' + newrst + ' restart onlyframes ' + str(frame) +'\n')
+                        f.write('parmwrite out ' + newTop +'\n')
+                        f.write('run \n')
+                        f.write('quit')
+                        f.close()
+	                count = count+1	
+	                print(count)
+                return
+                 
+        
